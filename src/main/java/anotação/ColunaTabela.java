351304dee508596cff34429afe01b9ea442af199
R4NP3R <ssrafa.2014@gmail.com>
2024-03-21 18:54:33 -0300

    first commit

---
+++ src/main/java/anotação/ColunaTabela.java
@@ -0,0 +1,13 @@
+package anotação;
+
+import java.lang.annotation.*;
+
+@Documented
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ColunaTabela {
+
+    String dbName();
+
+    String setJavaName();
+}
